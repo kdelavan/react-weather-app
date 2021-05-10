e0b985933ec5ba6bade5de31bc88be42dcac2e5e
kdelavan <78820584+kdelavan@users.noreply.github.com>
2021-05-10 12:21:28 -0700

    installed & imported bootstrap

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
      <h1>Weather App</h1>
-     <footer>Coded by Kathryn Delavan | Open-source on <a href="https://github.com">GitHub</a></footer>
+   
+     <footer>Coded by<a href="https://elegant-carson-775ffa.netlify.app/"> Kathryn Delavan</a> | Open-source on <a href="https://github.com/kdelavan/react-weather-app">GitHub</a></footer>
+     </div>
     </div>
   );
 }

d7ea5a5e4e148d3e375167334436d5ee969bdeea
kdelavan <78820584+kdelavan@users.noreply.github.com>
2021-05-18 14:11:51 -0700

    Created Forecast component & CSS

--- src/Weather.js
+++ src/Weather.js
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
 import axios from "axios";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
+import Rainbow from "./Rainbow";
+import Logo from "./images/VHS-logo.png";
 
 import "./Weather.css";
 
 export default function Weather(props) {
- 
-  const [weatherData, setWeatherData] = useState({ready: false});
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
   const [city, setCity] = useState(props.defaultCity);
+
   function handleResponse(response) {
     setWeatherData({
       ready: true,
+      coordinates: response.data.coord,
       temperature: response.data.main.temp,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
@@ -18,53 +23,55 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       city: response.data.name,
       icon: response.data.weather[0].icon,
-
     });
   }
 
-function search() {
-  const apiKey = "d714b403e950ff68e3e4666fcfff93ae"; 
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-  axios.get(apiUrl).then(handleResponse);
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  search();
-}
-
-function handleCityChange(event) {
-  setCity(event.target.value);
-}
-
-if (weatherData.ready) {
- return ( 
-   
- <div className="Weather container">
-  
-    <form onSubmit={handleSubmit}>
-       <input className="searchBar me-2 p-1"
-       type="search" 
-       placeholder="Enter a city" 
-       classname="form-control" 
-       onChange={handleCityChange}
-       />
-       <input className="btn btn-dark"
-       type="submit" 
-       value="Search" />
-     </form>
-      <WeatherInfo data={weatherData} />  
-     
-    </div>
-    
-  );
-
-} else {
-    search();
-    return "Loading...";
-}
+  function search() {
+    const apiKey = "d714b403e950ff68e3e4666fcfff93ae";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
-  
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
 
- 
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather container">
+        <form onSubmit={handleSubmit}>
+          <input
+            className="searchBar me-2 p-1"
+            type="search"
+            placeholder="Enter a city"
+            onChange={handleCityChange}
+          />
+          <input className="btn btn-dark" type="submit" value="Search" />
+        </form>
+
+        <div className="WeatherInfo">
+          <WeatherInfo data={weatherData} />
+          <WeatherForecast coordinates={weatherData.coordinates} />
+          <Rainbow />
+          <footer>
+            Coded by{" "}
+            <a href="https://elegant-carson-775ffa.netlify.app/">
+              Kathryn Delavan
+            </a>{" "}
+            | Open-source on{" "}
+            <a href="https://github.com/kdelavan/react-weather-app">GitHub</a>{" "}
+            <img className="img-fluid" src={Logo} alt="vhs logo" />
+          </footer>{" "}
+        </div>
+      </div>
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
+}
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -2,34 +2,25 @@ import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcons from "./WeatherIcons";
 import WeatherTemperature from "./WeatherTemperature";
-import Rainbow from "./Rainbow";
-import Logo from "./images/VHS-logo.png";
 
 export default function WeatherInfo(props) {
-    return (
-    <div className="WeatherInfo">
-      
-        <div className="city-temp">
-          <h1>{props.data.city} </h1>      
-            <span className="temperature"><WeatherIcons code={props.data.icon} />{" "}<WeatherTemperature fahrenheit={props.data.temperature} /> </span>
-       </div>
-        <FormattedDate date={props.data.date} />
-          <div className="d-flex flex-row-reverse">
-            <ul>
-              <li className="text-capitalize">{props.data.description}</li>
-              <li>Humidity: {props.data.humidity}%</li>
-              <li>Wind: {Math.round(props.data.wind)} mph</li>
-            </ul>
-          </div> 
-       <Rainbow />
-    <footer>Coded by{" "}
-      <a href="https://elegant-carson-775ffa.netlify.app/"> 
-      Kathryn Delavan</a> | Open-source on {" "}
-      <a href="https://github.com/kdelavan/react-weather-app">
-        GitHub</a>
-     {" "}<img className="img-fluid" src={Logo} alt="vhs logo"/>
-    </footer>
+  return (
+    <div className="WeatherInformation">
+      <div className="city-temp">
+        <h1>{props.data.city} </h1>
+        <span className="temperature">
+          <WeatherIcons code={props.data.icon} size={46} />{" "}
+          <WeatherTemperature fahrenheit={props.data.temperature} />{" "}
+        </span>
+      </div>
+      <FormattedDate date={props.data.date} />
+      <div className="d-flex flex-row-reverse">
+        <ul>
+          <li className="text-capitalize">{props.data.description}</li>
+          <li>Humidity: {props.data.humidity}%</li>
+          <li>Wind: {Math.round(props.data.wind)} mph</li>
+        </ul>
+      </div>
     </div>
-    
-    );
+  );
 }
